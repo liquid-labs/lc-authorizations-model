ec3d60bd6080d5c37516fcc19ae32c05790a11bb
Zane Rockenbaugh <zane@liquid-labs.com>
2019-08-06 19:41:12 -0500

    azn must be specified in a grant; ensure grant tuple unique

--- data/sql/schema/16-grants.schema.sql
+++ data/sql/schema/16-grants.schema.sql
@@ -1,12 +1,14 @@
 CREATE TABLE grants (
   id BIGINT,
   subject BIGINT,
-  azn BIGINT,
+  azn BIGINT NOT NULL,
   target BIGINT,
+  -- cookies VARCHAR(128)[], -- e.g. : "LIMIT:10" on create, etc.
   CONSTRAINT grants_key PRIMARY KEY ( id ),
   CONSTRAINT grants_subject_refs_users FOREIGN KEY ( subject ) REFERENCES users ( id ),
   CONSTRAINT grants_azn_refs_azns FOREIGN KEY ( azn ) REFERENCES azns ( id ),
-  CONSTRAINT grants_target_refs_entities FOREIGN KEY ( target ) REFERENCES entities ( id )
+  CONSTRAINT grants_target_refs_entities FOREIGN KEY ( target ) REFERENCES entities ( id ),
+  CONSTRAINT grants_single_grant UNIQUE ( subject, azn, target )
 );
 
 CREATE INDEX azns_index_subject ON grants ( subject );

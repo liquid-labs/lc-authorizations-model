74a59bf5594cc3690fe7f372124e638ce1099816
Zane Rockenbaugh <zane@liquid-labs.com>
2019-08-13 17:34:02 -0500

    update model; working

--- data/sql/schema/15-azns.schema.sql
+++ data/sql/schema/15-azns.schema.sql
@@ -7,9 +7,9 @@ CREATE TABLE azns (
 CREATE INDEX azns_index_name ON azns ( name );
 
 -- Notice there is no create; that's a per-type subject-less grant.
-INSERT INTO azns (0, '/entities/read');
-INSERT INTO azns (1, '/entities/read-sensitive');
-INSERT INTO azns (2, '/entities/write');
-INSERT INTO azns (3, '/entities/archive');
-INSERT INTO azns (4, '/entities/delete');
-INSERT INTO azns (5, '/entities/grant');
+INSERT INTO azns VALUES (0, '/entities/read');
+INSERT INTO azns VALUES (1, '/entities/read-sensitive');
+INSERT INTO azns VALUES (2, '/entities/update');
+INSERT INTO azns VALUES (3, '/entities/archive');
+INSERT INTO azns VALUES (4, '/entities/delete');
+INSERT INTO azns VALUES (5, '/entities/grant');
--- data/sql/schema/16-grants.schema.sql
+++ data/sql/schema/16-grants.schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE grants (
   target BIGINT,
   -- cookies VARCHAR(128)[], -- e.g. : "LIMIT:10" on create, etc.
   CONSTRAINT grants_key PRIMARY KEY ( id ),
-  CONSTRAINT grants_subject_refs_users FOREIGN KEY ( subject ) REFERENCES users ( id ),
+  CONSTRAINT grants_subject_refs_subjects FOREIGN KEY ( subject ) REFERENCES subjects ( id ),
   CONSTRAINT grants_azn_refs_azns FOREIGN KEY ( azn ) REFERENCES azns ( id ),
   CONSTRAINT grants_target_refs_entities FOREIGN KEY ( target ) REFERENCES entities ( id ),
   CONSTRAINT grants_single_grant UNIQUE ( subject, azn, target )

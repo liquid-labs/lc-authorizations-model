c3347ba558a4c0b0b3625726a9476a5e52583bd0
Zane Rockenbaugh <zane@liquid-labs.com>
2019-08-18 16:32:21 -0500

    formatting

--- data/sql/schema/15-user_groups.schema.sql
+++ data/sql/schema/15-user_groups.schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE user_groups (
   id UUID,
+  
   CONSTRAINT user_groups_key PRIMARY KEY ( id ),
   CONSTRAINT user_groups_refs_subjects FOREIGN KEY ( id ) REFERENCES subjects ( id ),
   CONSTRAINT user_groups_refs_containers FOREIGN KEY ( id ) REFERENCES containers ( id )
